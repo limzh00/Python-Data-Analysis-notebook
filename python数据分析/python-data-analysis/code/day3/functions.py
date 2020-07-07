26a76f7816bd7350ea9295b42f1e702d754eb396
limzh <liminzhang1013@gmail.com>
2020-07-07 17:36:35 +0800

    Update functions.py

--- python数据分析/python-data-analysis/code/day3/functions.py
+++ python数据分析/python-data-analysis/code/day3/functions.py
@@ -1,6 +1,18 @@
 # I would claim that `function` is object.
 # To understand that, some properties would be revealed.
 
-class Duck:
-    def __init__(self, age):
-        self.age
+class City:
+    def __init__(self, population, location, name):
+        self.population = population
+        self.location = location
+        self.is_lockdown = False
+        self.name = name
+    def lockdown(self):
+        self.is_lockdown = True
+        print("WARNING: " + self.name + " is at the LOCKDOWN state.")
+    def __call__(self):
+        print(10)
+    
+Tsingdao = City(9000000, 'China', 'Tsingdao')
+Tsingdao.lockdown()
+Tsingdao()
