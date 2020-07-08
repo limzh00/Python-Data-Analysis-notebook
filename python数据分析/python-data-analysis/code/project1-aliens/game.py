401d5bcca3b9ec6525b5a8a282fdd40f8f6049a6
limzh <liminzhang1013@gmail.com>
2020-07-08 15:56:34 +0800

    project1_code

--- python数据分析/python-data-analysis/code/project1-aliens/game.py
+++ python数据分析/python-data-analysis/code/project1-aliens/game.py
@@ -1,31 +1,61 @@
-#!/usr/bin/env python
-# -*- coding:UTF-8 -*-
-# AUTHOR: Minzhang Li
-# FILE: C:\Users\Administrator\Desktop\python数据分析\python基础语法以及配套练习(持续更新)-lmz\basic\day3\game.py
-# DATE: 2020/07/05 Sun
-# TIME: 19:38:35
+# 《外星人入侵》主程序
+import sys
+import pygame
+from pygame.sprite import Group
 
-# DESCRIPTION: This is a main game file for alien-invasion
+from setting import Settings
+from ship import Ship
+import game_functions as gf
+from alien import Alien
+from game_stats import GameStats
+from button import Button
+from scoreboard import ScoreBoard
 
-import pygame as pg
-import math
-from ship import Ship 
-from setting import Setting
 
 def run_game():
-    pg.init()
-    setting = Setting()
-    screen = pg.display.set_mode(
-        (setting.screen_width, setting.screen_height)
-    )
-    player = Ship(screen)
-    pg.display.set_caption("Alien Invasion")
+    # 初始化pygame 
+    pygame.init()
 
+    # 初始化游戏屏幕、游戏名设置，屏幕大小设置时要加括号
+    ai_settings = Settings() # 初始设置类的实例化
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+
+    # 创建一艘飞船、一个子弹编组和一个外星人组
+    ship = Ship(ai_settings, screen)
+    bullets = Group()
+    aliens = Group()
+
+    # 创建外星人群
+    gf.create_fleet(ai_settings, screen, ship, aliens)
+
+    # 创建Play按钮
+    play_button = Button(ai_settings, screen, "Play")
+
+    # 创建一个用于存储游戏统计信息的实例，并创建记分牌
+    stats = GameStats(ai_settings)
+    sb = ScoreBoard(ai_settings, screen, stats)
+
+    # 开始游戏的主循环
     while True:
-        screen.fill(setting.bg_color)
-        player.blitme()
-        pg.display.flip()
 
+        # 监视键盘和鼠标事件
+        gf.check_events(ai_settings, screen, stats, sb, play_button,
+            ship, aliens, bullets)
 
-if __name__ == '__main__':
-    run_game()
+        if stats.game_active:
+            # 更新飞机移动
+            ship.update()
+            # 更新子弹
+            gf.update_bullets(ai_settings, screen, stats, sb, ship,
+                    aliens,bullets)
+            # 更新外星人
+            gf.update_aliens(ai_settings, screen, stats, sb, ship,
+                aliens, bullets)
+
+        # 填充屏幕背景，飞机，并进行刷新
+        gf.update_screen(ai_settings, screen, stats, sb, ship,
+            aliens, bullets, play_button)
+
+run_game()
