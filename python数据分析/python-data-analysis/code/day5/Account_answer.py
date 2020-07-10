70922e71bda090491ca9b44cea2ea96fa4414c23
limzh <liminzhang1013@gmail.com>
2020-07-10 18:12:11 +0800

    day5

---
+++ python数据分析/python-data-analysis/code/day5/Account_answer.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding:UTF-8 -*-
+# AUTHOR: Minzhang Li
+# FILE: F:\MyGithubs\Python-Data-Analysis-notebook\python数据分析\python-data-analysis\code\day5\Account_answer.py
+# DATE: 2020/07/10 Fri
+# TIME: 18:06:23
+
+# DESCRIPTION: This is a answer file
+
+class Account:
+    def __init__(self, name, pwd):
+        self.holder = name
+        self.password = pwd
+        self.balance = 0
+    def deposit(self,num):
+        self.balance += num
+    def transfer(self, num):
+        self.balance -= num
+
+def transfer(A,B,num):
+    if A.balance >= num:
+        A.balance -= num
+        B.balance += num
+        return 1
+    print("TRANSFER FAIL: YOU DO NOT HAVE ENOUGH SPARE.")
+    return 0
