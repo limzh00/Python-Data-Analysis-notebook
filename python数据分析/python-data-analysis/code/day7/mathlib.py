a4ac966ec57f0c3f1916e27e14a4e5f3711621aa
limzh <liminzhang1013@gmail.com>
2020-07-12 22:45:15 +0800

    day7

---
+++ python数据分析/python-data-analysis/code/day7/mathlib.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding:UTF-8 -*-
+# AUTHOR: Minzhang Li
+# FILE: F:\MyGithubs\Python-Data-Analysis-notebook\python数据分析\python-data-analysis\code\day7\mathlib.py
+# DATE: 2020/07/12 Sun
+# TIME: 21:22:59
+
+# DESCRIPTION: This is mathlib file
+
+import math
+
+def add(x, y):
+    return x + y
+
+def sub(x, y):
+    return x - y
+
+def mul(x, y):
+    return x*y
+
+def div(x, y):
+    return x / y
+
+def rem(x, y):
+    return x % y
+
+def sqrt(x):
+    return math.sqrt(x)
+
+def power(x, y):
+    return math.pow(x, y)
+
+def sin(x):
+    return math.sin(x)
+
+def cos(x):
+    return math.cos(x)
