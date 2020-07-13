a8a7bf3938fe8eac89c7ba20c907c2166a435513
limzh <liminzhang1013@gmail.com>
2020-07-13 13:35:25 +0800

    Create examples1-10.py

---
+++ python数据分析/python-data-analysis/code/built-in-function/examples1-10.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding:UTF-8 -*-
+# AUTHOR: Minzhang Li
+# FILE: F:\MyGithubs\Python-Data-Analysis-notebook\python数据分析\python-data-analysis\code\built-in-function\examples1-10.py
+# DATE: 2020/07/13 Mon
+# TIME: 13:32:02
+
+# DESCRIPTION: None
+
+# 1.1 help( )
+
+
